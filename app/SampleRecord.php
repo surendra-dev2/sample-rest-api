0485798e3b37c67d8f0acd2046299a51246e342a
Surendra <schouksey@daenet.com>
2020-06-28 21:01:21 +0530

    models created.

---
+++ app/SampleRecord.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SampleRecord extends Model
+{
+    protected $table = 'sample_record';
+    public $timestamps = false;
+}
